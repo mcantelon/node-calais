680f3b84d96d674091b88b96c7f2e2b536de1b1c
Mike Cantelon <mcantelon@gmail.com>
2010-12-29 23:10:10 -0800

    Added a couple of tests.

---
+++ test/calais.test.js
@@ -0,0 +1,15 @@
+var Calais = require('calais').Calais,
+    assert = require('assert'),
+    should = require('should')
+
+module.exports = {
+  'test api key setting': function() {
+    var calais = new Calais('some_api_key')
+    calais.api_key.should.equal('some_api_key')
+  },
+
+  'test option setting': function() {
+    var calais = new Calais('some_api_key', {'cleanResult': false})
+    calais.options.cleanResult.should.equal(false)
+  },
+}
